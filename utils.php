ca7da0dbcc453c09339c8a6531fb24af703aa314
Karim Muhammad <kimoomar005@gmail.com>
2026-03-04 22:39:56 +0200

    feat: Implement  enable profile image uploads.

--- utils.php
+++ utils.php
@@ -2,37 +2,40 @@
 include_once 'Database.php';
 
 // Create Record in Database
-function createRecord($data) {
+function createRecord($data)
+{
     $connection = create_connection();
-    
+
     $unique_id = uniqid();
-    
+
     $stmt = $connection->prepare("
-        INSERT INTO users (id, firstname, lastname, country, address, gender, skills, username, password, department) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO users (id, firstname, lastname, country, address, gender, skills, username, password, department, profile_image) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     ");
     $stmt->execute([
         $unique_id,
         $data['firstname'],
-        $data['lastname'], 
+        $data['lastname'],
         $data['country'],
         $data['address'],
         $data['gender'],
         $data['skills'],
         $data['username'],
         $data['password'],
-        $data['department']
+        $data['department'],
+        $data['profile_image'] ?? ''
     ]);
     return $unique_id;
 }
 
 // Update Record in Database
-function updateRecord($id, $data) {
+function updateRecord($id, $data)
+{
     $connection = create_connection();
     $stmt = $connection->prepare("
         UPDATE users SET 
         firstname = ?, lastname = ?, country = ?, address = ?, 
-        gender = ?, skills = ?, username = ?, password = ?, department = ?
+        gender = ?, skills = ?, username = ?, password = ?, department = ?, profile_image = ?
         WHERE id = ?
     ");
     return $stmt->execute([
@@ -45,26 +48,30 @@ function updateRecord($id, $data) {
         $data['username'],
         $data['password'],
         $data['department'],
+        $data['profile_image'] ?? '',
         $id
     ]);
 }
 
 // Delete Record in Database
-function deleteRecord($id) {
+function deleteRecord($id)
+{
     $connection = create_connection();
     $stmt = $connection->prepare("DELETE FROM users WHERE id = ?");
     return $stmt->execute([$id]);
 }
 
 // Select All Records from Database
-function readAllRecords() {
+function readAllRecords()
+{
     $connection = create_connection();
     $stmt = $connection->query("SELECT * FROM users ORDER BY id DESC");
     return $stmt->fetchAll();
 }
 
 // Select Single Record from Database
-function readRecord($id) {
+function readRecord($id)
+{
     $connection = create_connection();
     $stmt = $connection->prepare("SELECT * FROM users WHERE id = ?");
     $stmt->execute([$id]);
@@ -72,7 +79,8 @@ function readRecord($id) {
 }
 
 // Check if username exists
-function usernameExists($username, $excludeId = null) {
+function usernameExists($username, $excludeId = null)
+{
     $connection = create_connection();
     if ($excludeId) {
         $stmt = $connection->prepare("SELECT COUNT(*) FROM users WHERE username = ? AND id != ?");
@@ -82,4 +90,13 @@ function usernameExists($username, $excludeId = null) {
         $stmt->execute([$username]);
     }
     return $stmt->fetchColumn() > 0;
+}
+
+// get user by username
+function getUserByUsername($username)
+{
+    $connection = create_connection();
+    $stmt = $connection->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    return $stmt->fetch();
 }
