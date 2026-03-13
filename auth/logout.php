0b18179efca2abea2cb8af56b667f47c39beafc7
Karim Muhammad <kimoomar005@gmail.com>
2026-03-13 12:44:20 +0200

    update login controller

--- auth/logout.php
+++ auth/logout.php
@@ -1,6 +1,11 @@
 <?php
+require_once __DIR__ . '/../utils.php';
+require_once __DIR__ . '/../app/Services/AuthService.php';
 
-session_start();
-session_destroy();
-header('Location: ../table.php');
+use App\Services\AuthService;
+
+$authService = new AuthService(getUserRepository());
+$authService->logout();
+
+header("Location: login.php");
 exit;
