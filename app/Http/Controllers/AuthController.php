fd3771cd1e253907a0d49d42aa78c2f73c9f3533
Karim Muhammad <kimoomar005@gmail.com>
2026-03-13 12:58:13 +0200

    move auth controllers to http

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Services\AuthService;
+
+class AuthController {
+    protected $authService;
+
+    public function __construct(AuthService $authService) {
+        $this->authService = $authService;
+    }
+
+    public function login() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $username = trim($_POST['username'] ?? '');
+        $password = trim($_POST['password'] ?? '');
+
+        $result = $this->authService->attemptLogin($username, $password);
+
+        if ($result['success']) {
+            $user = $result['user'];
+            header("Location: ../../view.php?id=" . urlencode($user['id']));
+            exit;
+        } else {
+            header('Location: ../login.php?error=' . urlencode($result['error']));
+            exit;
+        }
+    }
+
+    public function logout() {
+        $this->authService->logout();
+        header("Location: ../login.php");
+        exit;
+    }
+}
